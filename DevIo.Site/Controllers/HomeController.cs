16785de421e89c496041f8d2b0bf57a030323046
andre <andrefrateschi@gmail.com>
2020-09-24 16:51:00 -0300

    1
    Primeiro commit

---
+++ DevIo.Site/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIo.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+    public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DevIo.site/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevIo.site
+{
+    public class Class1
+    {
+    }
+}
